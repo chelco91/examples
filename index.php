e50223c3ff6acd9302f4439a1295de2de786592a
Christopher Collopy <chelco91@gmail.com>
2015-06-29 02:09:54 -0600

    Added css styles page and created link lists in the index page

--- index.php
+++ index.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta lang="en">
+		<meta lang="en" >
 		<title>Examples Galore</title>
+		<link href="css/styles.css" rel="stylesheet">
 	</head>
 	<body>
-		<?php
-		echo "<p>Hello World</p>";
-		?>
+		<form class="form">
+			<div class="title-bg">
+				<div class="title">Conditionals</div>
+			</div>
+			<div class="list-box">
+				<ul>
+					<li><a href="#">If Statement</a></li>
+					<li><a href="#">Else Statement</a></li>
+					<li><a href="#">ElseIf Statement</a></li>
+					<li><a href="#">Switch Statement</a></li>
+				</ul>
+			</div>
+		</form>
+		<form class="form">
+			<div class="title-bg">
+				<div class="title">Looping</div>
+			</div>
+			<div class="list-box">
+				<ul>
+					<li><a href="#">While Loop</a></li>
+					<li><a href="#">Do While Loop</a></li>
+					<li><a href="#">For Loop</a></li>
+				</ul>
+			</div>
+		</form>
+		<form class="form">
+			<div class="title-bg">
+				<div class="title">Objects</div>
+			</div>
+			<div class="list-box">
+				<ul>
+					<li><a href="#">Declaration</a></li>
+					<li><a href="#">Create and Access</a></li>
+					<li><a href="#">Constructors</a></li>
+					<li><a href="#">Destructors</a></li>
+					<li><a href="#">Constants</a></li>
+					<li><a href="#">Inheritance</a></li>
+				</ul>
+			</div>
+		</form>
 	</body>
 </html>
